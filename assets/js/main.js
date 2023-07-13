d8e000881fc1b3d63aa177852d19d2114e0f690c
Hwang-ji <ji881111@gmail.com>
2023-07-13 21:00:27 +0900

    코드수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,82 @@
 // show menu
-const navMenu = document.getElementById('nav-menu')
-const navToggle = document.getElementById('nav-toggle')
-const navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
-if(navToggle){
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu')
-  })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
-if(navClose){
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu')
-  })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 // remove menu mobile
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach(n => n.addEventListener("click", linkAction));
 
 // add blur to header
 const blurHeader = () => {
-  const header = document.getElementById('header')
-  
-  this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader)
+  const header = document.getElementById("header");
+
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
 
 // show scroll up
 const scrollUp = () => {
-  const scrollUpBtn = document.getElementById('scroll-up')
-  this.scrollY >= 350 ? scrollUpBtn.classList.add('show-scroll') : scrollUpBtn.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollUp)
+  const scrollUpBtn = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUpBtn.classList.add("show-scroll")
+    : scrollUpBtn.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 // scroll sections active link
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
 const scrollActive = () => {
-  const scrollY = window.pageYOffset
-  
+  const scrollY = window.scrollY;
+
   sections.forEach(current => {
     const sectionHeight = current.offsetHeight,
-          sectionTop = current.offsetTop - 58,
-          sectionId = current.getAttribute('id'),
-          sectionsClass = document.querySelector(`.nav__menu a[href*=${sectionId}]`)
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        `.nav__menu a[href*=${sectionId}]`
+      );
 
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-      sectionsClass.classList.add('active-link')
-    }else{
-      sectionsClass.classList.remove('active-link')
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
     }
-  })
-}
-window.addEventListener('scroll', scrollActive)
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 // scroll reveal animation
 const sr = ScrollReveal({
-  origin: 'top',
-  distance: '60px',
+  origin: "top",
+  distance: "60px",
   duration: 3000,
   delay: 400,
   //reset: true //Animations repeat
-})
+});
 
-sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`)
-sr.reveal(`.home__card`, {delay: 600, distance: '100px', interval: 100})
-sr.reveal(`.about__data, .join__image`, {origin: 'right'})
-sr.reveal(`.about__image, .join__data`, {origin: 'left'})
-sr.reveal(`.product__card`, {interval: 200})
+sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`);
+sr.reveal(`.home__card`, { delay: 600, distance: "100px", interval: 100 });
+sr.reveal(`.about__data, .join__image`, { origin: "right" });
+sr.reveal(`.about__image, .join__data`, { origin: "left" });
+sr.reveal(`.product__card`, { interval: 200 });
